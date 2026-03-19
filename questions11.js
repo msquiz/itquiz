30a47177f3d52a29da0328404512e7f7c4e31bbb
msquiz <mastersahebmail@gmail.com>
2026-03-19 08:55:37 +0530

    Add files via upload

---
+++ questions11.js
@@ -0,0 +1,12 @@
+const questionBank11 = [
+{
+q:"Computer ka full form kya hai?",
+options:["Common Operating Machine","Central Operating Machine","Common Operating System","None"],
+ans:0
+},
+{
+q:"CPU ka kaam kya hai?",
+options:["Processing","Storage","Printing","Scanning"],
+ans:0
+}
+];
